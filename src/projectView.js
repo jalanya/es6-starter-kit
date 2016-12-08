e8e34d5866f7e7624c22b7b4f1575b8f0f550d39
Jose Alanya <jose@redisrupt.io>
2016-12-08 13:51:14 -0500

    Add messages

--- src/projectView.js
+++ src/projectView.js
@@ -2,4 +2,8 @@ export let project = { projectId: 99};
 
 export function showProject() {
   console.log(project.projectId);
+  console.log('change 1');
+  console.log('change 2');
+  console.log('change 3');
+  console.log('change 4');
 }
