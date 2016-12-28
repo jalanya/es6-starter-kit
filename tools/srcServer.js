97d7ea84b6f6a9aeb13fb82a23b39050fd5c8fc7
Jose Alanya <jose.alanya@gmail.com>
2016-12-28 18:06:11 -0500

    TST: Apply lints fixes

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -12,16 +12,16 @@ const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
